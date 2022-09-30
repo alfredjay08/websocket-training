383812216b44062776a9ea531e54705b2764fcf7
alfredjay08 <54141766+alfredjay08@users.noreply.github.com>
2022-09-30 14:08:16 +0800

    Websocket Message Mirror

---
+++ src/js/app.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const wsUri = "wss://ws.vi-server.org/mirror";
+const websocket = new WebSocket(wsUri);
+const form = document.getElementById("form__message");
+
+let output;
+
+const init = () => {
+  output = document.getElementsByClassName("message__container")[0];
+
+  websocket.onopen = (e) => onOpen(e);
+  websocket.onclose = (e) => onClose(e);
+  websocket.onmessage = (e) => onMsg(e);
+  websocket.onerror = (e) => onError(e);
+};
+
+const onOpen = (e) => {
+  console.log(e);
+  writeMsg("CONNECTED to the Platform");
+  sendMsg(`WebSockets r the cool`);
+};
+
+const onClose = (e) => {
+  writeMsg("DISCONNECTED to the Platform");
+};
+
+const onMsg = (e) => {
+  writeMsg(`<p style='color: blue;'>RESPONSE: ${e.data}`);
+};
+
+const onError = (e) => {
+  writeMsg(`<p style='color: red;'>ERROR: ${e.data}`);
+};
+
+const sendMsg = (msg) => {
+  writeMsg(`SENT: ${msg}`);
+  websocket.send(msg);
+};
+
+const sendMessage = (e) => {
+  e.preventDefault();
+
+  const { message } = Object.fromEntries([...new FormData(e.currentTarget)]);
+
+  sendMsg(`You: ${message}`);
+};
+
+const writeMsg = (msg) => {
+  const text = document.createElement("p");
+  text.innerHTML = msg;
+  output.appendChild(text);
+};
+
+form.addEventListener("submit", sendMessage);
+window.addEventListener("load", init, false);
